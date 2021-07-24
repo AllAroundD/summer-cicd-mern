4a38648804f0bef275c9257bd136428a646eac5a
AllAroundD <AllAroundDWeb@gmail.com>
2021-07-23 21:53:07 -0400

    added initial cypress testing

---
+++ client/cypress/integration/home/app.spec.js
@@ -0,0 +1,14 @@
+/// <reference types="cypress" />
+
+describe("homepage with default data", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3001");
+  });
+
+  it("displays three restaurants", () => {
+    cy.get("#restaurants-display").children().should("have.length", 3);
+  });
+  it("updates the input value", () => {
+    cy.get("#name").type("Waffle House");
+  });
+});
