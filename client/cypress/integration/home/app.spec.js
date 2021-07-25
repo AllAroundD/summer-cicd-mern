016712369df9aa57376786d485281736b1223ac1
AllAroundD <AllAroundDWeb@gmail.com>
2021-07-24 22:03:45 -0400

    added remaining cypress tests

--- client/cypress/integration/home/app.spec.js
+++ client/cypress/integration/home/app.spec.js
@@ -3,6 +3,7 @@
 
 describe("homepage with default data", () => {
   beforeEach(() => {
+    cy.exec("cd .. && npm run seed");
     cy.visit("http://localhost:3001");
   });
 
@@ -10,6 +11,12 @@ describe("homepage with default data", () => {
     cy.get("#restaurants-display").children().should("have.length", 3);
   });
   it("updates the input value", () => {
-    cy.get("#name").type("Waffle House");
+    const nameToTest = "Banana";
+    cy.get("#name").type(nameToTest);
+    cy.get("#name").should("have.have.value", nameToTest);
+  });
+  it("should submit the form", () => {
+    cy.get("#name").type("Waffle House{enter}");
+    cy.get("#restaurants-display").children().should("have.length", 4);
   });
 });
